a2a47d5f57e45378bd4bf50c601aaf0af849f76a
Vladislav <32579343+vblago@users.noreply.github.com>
2017-11-15 14:58:54 +0200

    Revert "Merge branch 'master' of https://github.com/vblago/Surgule"
    This reverts commit 069a20659f5dd577cf0a99d5b6aead58a110fe2c, reversing
    changes made to 87f4dc7dfe704b0a39250964a70ea18af4d1cbc8.

--- views/product/view.php
+++ views/product/view.php
@@ -1,6 +1,5 @@
-<?php include ROOT . '../views/layouts/header.php'; ?>
+<?php include ROOT . '/views/layouts/header.php'; ?>
 
-<<<<<<< HEAD
 
     <div class="product-big-title-area">
             <div class="container">
@@ -9,26 +8,6 @@
                         <div class="product-bit-title text-center">
                             <h2>Shop</h2>
                         </div>
-=======
-<section>
-    <div class="container" style="margin-top: 20px; margin-bottom: 20px">
-        <div class="row">
-            <div class="col-sm-3">
-                <div class="left-sidebar">
-<!--                    <h1>single prod</h1>-->
-                    <div class="panel-group category-products">
-                        <?php foreach ($categories as $categoryItem): ?>
-                            <div  class="panel panel-default">
-                                <div id="cat" class="panel-heading">
-                                    <h4 class="panel-title">
-                                        <a href="/category/<?php echo $categoryItem['id']; ?>">
-                                            <?php echo $categoryItem['name']; ?>
-                                        </a>
-                                    </h4>
-                                </div>
-                            </div>
-                        <?php endforeach; ?>
->>>>>>> d383c968f8db705c573e14346764157bf3368f46
                     </div>
                 </div>
             </div>
@@ -37,7 +16,6 @@
             <div class="zigzag-bottom"></div>
                 <div class="container">
                     <div class="row">
-<<<<<<< HEAD
                         <div class="col-sm-3">
                             <div class="left-sidebar">
                                 <h2>Каталог</h2>
@@ -53,11 +31,6 @@
                                         </div>
                                     </div>
                                 <?php endforeach; ?>
-=======
-                        <div class="col-sm-5">
-                            <div class="view-product">
-                                <img  src="<?php echo Product::getImage($product['id']); ?>" alt="" />
->>>>>>> d383c968f8db705c573e14346764157bf3368f46
                             </div>
                         </div>
                     </div>
@@ -118,7 +91,6 @@
                                             </div>
                                         </div>
 
-<<<<<<< HEAD
                                         <h2><a href="">Sony Smart TV - 2015</a></h2>
 
                                         <div class="product-carousel-price">
@@ -133,37 +105,6 @@
                                                 <a href="" class="view-details-link"><i class="fa fa-link"></i> See details</a>
                                             </div>
                                         </div>
-=======
-                                <?php if ($product['is_new']): ?>
-                                    <img  src="/template/images/product-details/new.jpg" class="newarrival" alt="" />
-                                <?php endif; ?>
-
-                                <h2><?php echo $product['name']; ?></h2>
-                                <p>Код товара: <?php echo $product['code']; ?></p>
-                                <span>
-                                    <span>US $<?php echo $product['price']; ?></span>
-                                    
-                                </span>
-                                
-                                <p><b>Наличие:</b> <?php echo Product::getAvailabilityText($product['availability']); ?></p>
-                                <p><b>Производитель:</b> <?php echo $product['brand']; ?></p>
-                                <p><a href="#" data-id="<?php echo $product['id']; ?>"
-                                       class="btn btn-default add-to-cart">
-                                        <i class="fa fa-shopping-cart"></i>В корзину
-                                    </a>
-                                </p>
-                            </div><!--/product-information-->
-                        </div>
-                    </div>
-                    <div class="row">                                
-                        <div class="col-sm-12">
-                            <br/>
-                            <h5>Описание товара</h5>
-                            <?php echo $product['description']; ?>
-                        </div>
-                    </div>
-                </div><!--/product-details-->
->>>>>>> d383c968f8db705c573e14346764157bf3368f46
 
                                         <h2><a href="">Apple new mac book 2015 March :P</a></h2>
                                         <div class="product-carousel-price">
