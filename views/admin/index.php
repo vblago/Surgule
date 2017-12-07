05ed8ffb3ea4579539b953eb9f2f731a00c91e3b
elkavitaly <32566998+elkavitaly@users.noreply.github.com>
2017-12-07 19:08:43 +0200

    admin
    main page of admin panel

--- views/admin/index.php
+++ views/admin/index.php
@@ -1,26 +1,29 @@
 <?php include ROOT . '/views/layouts/header_admin.php'; ?>
-
-<section>
+<style type="text/css">
+    li{
+        margin: 10px;
+    }
+</style>
+<section style="padding: 20px;">
+    <div align="center" >
     <div class="container">
         <div class="row">
             
             <br/>
             
-            <h4>Добрый день, администратор!</h4>
+            <h2 style="color: #1108BE; font-size: 30px">Добрый день, администратор!</h2>
             
-            <br/>
+            <h4 style="color: #9B1C77; font-size: 25px">Вам доступны такие возможности:</h4>
+        <ul type="none">        
+            <li><a  href="/admin/product" style="text-decoration: none; background-color: #FC4605; color: white; font-size: 25px;">Управление товарами</a><br/></li>
+             
+            <li><a style="text-decoration: none; background-color: #FC4605; color: white; font-size: 25px" href="/admin/category">Управление категориями</a><br/></li>
             
-            <p>Вам доступны такие возможности:</p>
-            
-            <br/>
-            
-            <ul>
-                <li><a href="/admin/product">Управление товарами</a></li>
-                <li><a href="/admin/category">Управление категориями</a></li>
-                <li><a href="/admin/order">Управление заказами</a></li>
-            </ul>
-            
+            <li><a style="text-decoration: none; background-color: #FC4605; color: white; font-size: 25px" href="/admin/order">Управление заказами</a></li>
+           
+         </ul>   
         </div>
+    </div>
     </div>
 </section>
 
