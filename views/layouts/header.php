bd8d6c8402e8f9527bacf07ef0f24206af1d41fb
elkavitaly <32566998+elkavitaly@users.noreply.github.com>
2017-12-10 23:46:08 +0200

    Выделение элементов меню

--- views/layouts/header.php
+++ views/layouts/header.php
@@ -26,13 +26,16 @@
     <link rel="stylesheet" href="/template/css/style.css">
     <link rel="stylesheet" href="/template/css/responsive.css">
    
+
+    
+
   </head>
-  <body>
-   
+  <body style="padding: 0; margin: 0;">
+    <div style="padding: 0px; margin: -20px;">
     <div class="header-area">
         <div class="container">
             <div class="row">
-                <div class="col-md-8">
+                <div class="col-md-8" >
                     <div class="user-menu">
                         <ul>
                             <li><a href="#"><i class="fa fa-user"></i> Мій запис</a></li>
@@ -66,7 +69,7 @@
             </div>
         </div>
     </div> <!-- End header area -->
-    
+    </div>
     <div style="height: 110px" class="site-branding-area">
         <div class="container">
             <div class="row">
@@ -90,13 +93,24 @@
             <div class="row"> 
                 <div class="navbar-collapse collapse">
                     <ul class="nav navbar-nav">
-                        <li class="active"><a href="/">Домашня</a></li>
-                        <li><a href="/catalog/">Магазин</a></li>
-                        <li><a href="/contacts/">Контакти</a></li>
+                        <li class="<?php if (getUrl() == "http://localhost/") echo active?>"><a href="/">Домашня</a></li>
+                        <li class="<?php if (getUrl() == "http://localhost/catalog/") echo active?>"><a href="/catalog/">Магазин</a></li>
+                        <li class="<?php if (getUrl() == "http://localhost/contacts/") echo active?>"><a href="/contacts/">Контакти</a></li>
                         <li><a href="user/register">Single product</a></li>
-                        <li> <a href="/product/<?php echo 45;?>"></a></li>
+                        <li><a href="/product/<?php echo 45;?>"></a></li>
                     </ul>
                 </div>  
             </div>
         </div>
     </div> <!-- End mainmenu area -->
+<?php
+    function getUrl() {
+      $url  = @( $_SERVER["HTTPS"] != 'on' ) ? 'http://'.$_SERVER["SERVER_NAME"] :  'https://'.$_SERVER["SERVER_NAME"];
+      $url .= ( $_SERVER["SERVER_PORT"] != 80 ) ? ":".$_SERVER["SERVER_PORT"] : "";
+      $url .= $_SERVER["REQUEST_URI"];
+      
+      $st=strval($url);
+      return $st;
+    }
+    
+?>
