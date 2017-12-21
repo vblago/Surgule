0c42d3f94b256db0bec484f54e796dd53d75d8f5
elkavitaly <32566998+elkavitaly@users.noreply.github.com>
2017-12-21 13:24:37 +0200

    mail
    1 запрос

--- views/user/recovery.php
+++ views/user/recovery.php
@@ -1,15 +1,15 @@
 <?php include ROOT . '/views/layouts/header1.php'; ?>
 <div class="RecBlock">
 	<?php if (isset($errors) && is_array($errors)): ?>
-                    <ul>
-                        <?php foreach ($errors as $error): ?>
-                            <li> - <?php echo $error; ?></li>
-                        <?php endforeach; ?>
-                    </ul>
-                <?php endif; ?>
+       	<ul>
+            <?php foreach ($errors as $error): ?>
+                <li> - <?php echo $error; ?></li>
+            <?php endforeach; ?>
+        </ul>
+    <?php endif; ?>
 		<h1 class="titleRec" ">Восстановление пароля</h1><br>
 	 	
-	 	<form action="<?php UserController::actionRecovery(); ?>" method="post" class="formRec">
+	 	<form action="#" method="post" class="formRec">
 	        <label style="font-size: 18px;">Введите ваш e-mail:</label><br><input type="text" name="email" class="formInp" placeholder="E-mail">
 	        <br><br>
 	        <p><input type="submit" name="submit" class="btn btn-default" value="Восстановить пароль" ></p><br>
