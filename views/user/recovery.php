77f65819eef0dc31e45ff645eb82feae33202982
RomanMykh <32567418+RomanMykh@users.noreply.github.com>
2017-12-21 12:07:16 +0200

    восстановление пароля
    стиль

--- views/user/recovery.php
+++ views/user/recovery.php
@@ -1,9 +1,12 @@
 <?php include ROOT . '/views/layouts/header1.php'; ?>
-		<h1 class="titleRec" ">Восстановление пароля</h1>
+<div class="RecBlock">
+		<h1 class="titleRec" ">Восстановление пароля</h1><br>
 	 	
 	 	<form action="<?php UserController::actionRecovery(); ?>" method="post" class="formRec">
-	        <input type="text" name="email" class="formInp">
-	        <p><input type="submit" name="submit" class="btn btn-default" value="Восстановить пароль" ></p>
+	        <label style="font-size: 18px;">Введите ваш e-mail:</label><br><input type="text" name="email" class="formInp" placeholder="E-mail">
+	        <br><br>
+	        <p><input type="submit" name="submit" class="btn btn-default" value="Восстановить пароль" ></p><br>
 
 	   	</form>	
+</div>
 <?php include ROOT . '/views/layouts/footer.php'; ?>
