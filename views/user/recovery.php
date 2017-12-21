6975facebc5cea9fcd84774166fc05f23a8785f9
RomanMykh <32567418+RomanMykh@users.noreply.github.com>
2017-12-21 11:40:00 +0200

    Recovery
    изменения

--- views/user/recovery.php
+++ views/user/recovery.php
@@ -1,12 +1,9 @@
 <?php include ROOT . '/views/layouts/header1.php'; ?>
- 	
-	<h1 style="margin: 20px;">Восстановление пароля</h1>
- 	
- 	<form action="<?php UserController::actionRecovery(); ?>" method="post" style="margin: 30px;">
-        <input type="text" name="email" style="width: 300px; margin-bottom: 20px;">
-        <p><input type="submit" name="submit" class="btn btn-default" value="Восстановить пароль" ></p>
+		<h1 class="titleRec" ">Восстановление пароля</h1>
+	 	
+	 	<form action="<?php UserController::actionRecovery(); ?>" method="post" class="formRec">
+	        <input type="text" name="email" class="formInp">
+	        <p><input type="submit" name="submit" class="btn btn-default" value="Восстановить пароль" ></p>
 
-   	</form>
-
-   			
+	   	</form>	
 <?php include ROOT . '/views/layouts/footer.php'; ?>
